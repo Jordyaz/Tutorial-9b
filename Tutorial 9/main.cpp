f2d4107685428f9a5e7fd6f73ab41647a99411f6
Jorge Romero Díaz <jrd.1958@gmail.com>
2022-05-29 12:44:41 -0600

    Agregar archivos de proyecto.

---
+++ Tutorial 9/main.cpp
@@ -0,0 +1,30 @@
+//**********************************************************************************//
+//Tutirial 9  -  Github                                //   
+//**********************************************************************************//
+//                                                                                  //                    
+// General    > Dir salida:           >  $(SolutionDir)\Bin                         //
+//                 > Dir intermedio:  >  $(SolutionDir)\Tmp                         //
+//                 > Nomb Destino:    >  $(ProjectName)            (Release / 64)   //
+//                 > Nomb Destino:    >  $(ProjectName)_d          (Debug /64)      //
+//                 > Nomb Destino:    >  $(ProjectName)_32         (Release / 32)   //
+//                 > Nomb Destino:    >  $(ProjectName)_32d        (Debug / 32)     // 
+//                                                                                  //                         
+// Avanzado > Juego de caract  >  Multibyte                                         //
+// C/C++      > General               >  Comp multiproc          > Si               //
+//                 > Optimizacion     >  Tamaño o velocidad      > Codigo rapido    //
+//                 > Gen de codigo    >  Bib en tiempo de eje    > MT    (Release)  //
+//                 >                  >  Bib en tiempo de eje    > MTd  (Debug)     //
+//                 >                  >  Modelo de punt flota    > Rapido           //
+//                 > Idioma           >  Estandar del Lengua     > Ultimo Borrador  //
+//
+//**********************************************************************************// 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hola Mundo";
+	cin.get();
+	return 0;
+}
